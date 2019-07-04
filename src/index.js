c38a540ba0400b8ae979d46f6e2e59e259ac9b1b
Daniel Butler <daniel@dbutlercpa.com>
2019-07-04 13:05:12 -0400

    rearranged to flow better

--- src/index.js
+++ src/index.js
@@ -121,12 +121,6 @@ class Game extends React.Component {
     }
 }
 
-// ============================================
-
-ReactDOM.render(
-    <Game />, document.getElementById('root')
-);
-
 function calculateWinner(squares){
     const lines = [
         [0, 1, 2],
@@ -146,3 +140,9 @@ function calculateWinner(squares){
     }
     return null;
 }
+
+// ============================================
+
+ReactDOM.render(
+    <Game />, document.getElementById('root')
+);
